b14ff39b10f9ea0bb2b802b2cecabf9bd47cc608
formegusto <formegusto@gmail.com>
2021-03-29 12:41:26 +0900

    pug - variables, loop, condition

--- 05_express/routes/pug.js
+++ 05_express/routes/pug.js
@@ -6,4 +6,36 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/variables", (req, res) => {
+  res.render("variables", {
+    title: "변수",
+  });
+});
+
+router.get("/loop", (req, res) => {
+  res.render("loop", {
+    title: "반복문",
+    fruits: ["사과", "배", "오렌지", "바나나", "복숭아"],
+  });
+});
+
+router.get("/condition", (req, res) => {
+  res.render("condition", {
+    title: "조건문",
+  });
+});
+
+router.get("/case", (req, res) => {
+  res.render("case", {
+    title: "케이스문",
+  });
+});
+
+/*
+router.get("/variables", (req, res, next) => {
+  res.locals.title = "변수";
+  res.render("variables");
+});
+*/
+
 module.exports = router;
