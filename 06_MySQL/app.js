db0fa286ac765dbcee4bffbd9852f0ee855b4c09
formegusto <formegusto@gmail.com>
2021-04-04 17:19:40 +0900

    Sequelize Clear

--- 06_MySQL/app.js
+++ 06_MySQL/app.js
@@ -8,7 +8,7 @@ const { sequelize } = require("./models");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
-nunjucks.configure({
+nunjucks.configure("views", {
   express: app,
   watch: true,
 });
@@ -26,6 +26,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
+const commentsRouter = require("./routes/comments");
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/comments", commentsRouter);
+
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
@@ -36,7 +44,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render("views/error");
+  res.render("error");
 });
 
 app.listen(app.get("port"), () => {
