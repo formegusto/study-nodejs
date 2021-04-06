875ea3c2ab1aa27321f42f7217565621d1bb1838
formegusto <formegusto@gmail.com>
2021-04-06 22:30:40 +0900

    auth

--- 08_sns/app.js
+++ 08_sns/app.js
@@ -5,11 +5,14 @@ const path = require("path");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
+const passport = require("passport");
 
 dotenv.config();
 const pageRouter = require("./routes/page");
+const passportConfig = require("./passport");
 
 const app = express();
+passportConfig(); // 패스포트 설정
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
 
@@ -45,7 +48,13 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", pageRouter);
+
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
