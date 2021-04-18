a3af94b43e15dbe25ce47af47728b6a0cf23cfad
formegusto <formegusto@gmail.com>
2021-04-18 23:56:49 +0900

    socket

--- 11_2_socket_io/socket.js
+++ 11_2_socket_io/socket.js
@@ -1,6 +1,7 @@
-const { default: axios } = require("axios");
 const SocketIO = require("socket.io");
 const axios = require("axios");
+const cookieParser = require("cookie-parser");
+const cookie = require("cookie-signature");
 
 module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: "/socket.io" });
@@ -9,6 +10,11 @@ module.exports = (server, app, sessionMiddleware) => {
   const chat = io.of("/chat");
 
   io.use((socket, next) => {
+    // cookieParser(process.env.COOKIE_SECRET)(
+    //   socket.request,
+    //   socket.request.res,
+    //   next
+    // );
     sessionMiddleware(socket.request, socket.request.res, next);
   });
 
@@ -29,6 +35,8 @@ module.exports = (server, app, sessionMiddleware) => {
       .split("/")
       [referer.split("/").length - 1].replace(/\?.+/, "");
     socket.join(roomId);
+
+    console.log(req);
     socket.to(roomId).emit("join", {
       user: "system",
       chat: `${req.session.color}님이 입장하셨습니다.`,
@@ -40,8 +48,14 @@ module.exports = (server, app, sessionMiddleware) => {
       const currentRoom = socket.adapter.rooms[roomId];
       const userCount = currentRoom ? currentRoom.length : 0;
       if (userCount === 0) {
+        const signedCookie = req.signedCookie["connect.sid"];
+        const connectSID = cookie.sign(signedCookie, process.env.COOKIE_SECRET);
         axios
-          .delete(`http://localhost:8005/room/${roomId}`)
+          .delete(`http://localhost:8005/room/${roomId}`, {
+            headers: {
+              Cookie: `connect.sid=s%3A${connectSID}`,
+            },
+          })
           .then(() => {
             console.log("방 제거 요청 성공");
           })
