f065be3627c7acd82328ed36cddf0a3af75533ed
formegusto <formegusto@gmail.com>
2021-04-26 17:20:47 +0900

    낙찰 리스트~

--- 12_NodeAuction/routes/index.js
+++ 12_NodeAuction/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const schedule = require("node-schedule");
 
 const { Good, Auction, User } = require("../models");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
@@ -57,6 +58,7 @@ const upload = multer({
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
+
 router.post(
   "/good",
   isLoggedIn,
@@ -64,12 +66,33 @@ router.post(
   async (req, res, next) => {
     try {
       const { name, price } = req.body;
-      await Good.create({
+      const good = await Good.create({
         OwnerId: req.user.id,
         name,
         img: req.file.filename,
         price,
       });
+      const end = new Date();
+      end.setDate(end.getDate() + 1);
+      schedule.scheduleJob(end, async () => {
+        const success = await Auction.findOne({
+          where: { GoodId: good.id },
+          order: [["bid", "DESC"]],
+        });
+        await Good.update(
+          { SoldId: success.UserId },
+          { where: { id: good.id } }
+        );
+        await User.update(
+          {
+            money: sequelize.literal(`money - ${success.bid}`),
+          },
+          {
+            where: { id: success.UserId },
+          }
+        );
+      });
+
       res.redirect("/");
     } catch (err) {
       console.error(err);
@@ -139,6 +162,20 @@ router.post("/good/:id/bid", isLoggedIn, async (req, res, next) => {
   } catch (err) {
     console.error(err);
     return next(err);
+  }
+});
+
+router.get("/list", isLoggedIn, async (req, res, next) => {
+  try {
+    const goods = await Good.findAll({
+      where: { SoldId: req.user.id },
+      include: { model: Auction },
+      order: [[{ model: Auction }, "bid", "DESC"]],
+    });
+    res.render("list", { title: "낙찰 목록 - NodeAuction", goods });
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
 });
 
