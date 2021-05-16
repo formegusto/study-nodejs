19815ff51eeebb0d248e373a17734a0997721742
formegusto <formegusto@gmail.com>
2021-05-16 11:51:01 +0900

    Multer

--- 15_serverless/fileUploads/app.js
+++ 15_serverless/fileUploads/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 const path = require("path");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { sequelize } = require("./models");
 
@@ -26,6 +27,8 @@ sequelize
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/img", express.static(path.join(__dirname, "upload")));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const indexRouter = require("./routes");
 
